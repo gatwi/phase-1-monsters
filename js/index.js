a4bf7957547976d1a3dc38af790a1beef3c0f01e
gatwi <faith.murithi@student.moringaschool.com>
2022-06-20 08:12:33 +0300

    commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+POST http://localhost:3000/monsters
+headers:
+{
+    "Content-Type"; "application/json",
+    Accept; "application/json"
+}
+
+body:
+{
+    "name"; "Ella",
+    "age"; 1739.92794998293933,
+    "description"; "Oldest child of Odin. Goddess of death."
+    "id"; 1
+}
